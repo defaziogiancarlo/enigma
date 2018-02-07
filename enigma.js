80df250d8d432901278b0c02e131ab4a4cb47d8e
Gian-Carlo DeFazio <defaziogiancarlo@gmail.com>
2018-02-07 04:18:02 -0800

    added javascript and css files, have an input and output for text

---
+++ enigma.js
@@ -0,0 +1,6 @@
+
+document.getElementById("input-button").onclick = function() {
+  var inputVal = document.getElementById("input-box").value;
+  document.getElementById("output-box").value = inputVal;
+  console.log("hey");
+}
